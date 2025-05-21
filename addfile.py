dcde9a7b8df90d234127369f58d1182650964df6
Simone Romualdi <simoneromualdi9@gmail.com>
2025-05-21 19:33:10 +0200

    Update addfile.py

--- addfile.py
+++ addfile.py
@@ -1,6 +1,6 @@
-#Lo scopo dell'eseguibile è spostare un singolo file (che si trova nella cartella files) nella sottocartella di competenza, aggiornando il recap. 
+#/Lo scopo dell'eseguibile è spostare un singolo file (che si trova nella cartella files) nella sottocartella di competenza, aggiornando il recap. 
 L'interfaccia dell'eseguibile ha come unico argomento (obbligatorio) il nome del file da spostare (comprensivo di formato, es: 'trump.jpeg'). 
-Nel caso in cui il file passato come argomento non esista, l'interfaccia deve comunicarlo all'utente.#
+Nel caso in cui il file passato come argomento non esista, l'interfaccia deve comunicarlo all'utente./#
 
 
 import os
